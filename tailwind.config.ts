5f55f79f681d61b0e4561127da203be72943256b
zdangz <128911317+zdangz@users.noreply.github.com>
2025-11-12 16:46:02 +0800

    Zd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,14 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: 'media',
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
